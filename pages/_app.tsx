d2f0a01f30b1e9a129b62a66f47b78ab59461dc1
esgungor <engin.gungor14@gmail.com>
2022-01-04 14:16:26 +0100

    main configuration for kc #1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,37 @@
 import type { AppProps } from "next/app";
 import "semantic-ui-css/semantic.min.css";
+import { SSRCookies, SSRKeycloakProvider } from "@react-keycloak/ssr";
+import getConfig from "next/config";
+import { KeycloakConfig } from "keycloak-js";
+
 import Drawer from "../components/sidebar/sidebar";
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface AppPropsWithCookies extends AppProps {
+  cookies: unknown;
+}
+
+function MyApp({ Component, pageProps, cookies }: AppPropsWithCookies) {
+  const { publicRuntimeConfig } = getConfig();
+  const keycloakCfg: KeycloakConfig = {
+    realm: publicRuntimeConfig.keycloak.realm,
+    url: publicRuntimeConfig.keycloak.url,
+    clientId: publicRuntimeConfig.keycloak.clientId,
+  };
+  const keycloakInitOptions = {
+    onLoad: "check-sso",
+    silentCheckSsoRedirectUri: `${publicRuntimeConfig.baseUrl}/silent-check-sso.html`,
+    pkceMethod: "S256",
+  };
   return (
-    <Drawer>
-      <Component {...pageProps} />
-    </Drawer>
+    <SSRKeycloakProvider
+      keycloakConfig={keycloakCfg}
+      persistor={SSRCookies(cookies)}
+      initOptions={keycloakInitOptions}
+    >
+      <Drawer>
+        <Component {...pageProps} />
+      </Drawer>
+    </SSRKeycloakProvider>
   );
 }
 

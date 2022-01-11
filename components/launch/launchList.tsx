e58483b6f42ca5c8157aa0f54adcc37cf0924e87
esgungor <engin.gungor14@gmail.com>
2022-01-11 06:34:59 +0100

    add workload status metric to table #7

--- components/launch/launchList.tsx
+++ components/launch/launchList.tsx
@@ -2,9 +2,9 @@ import { useKeycloak } from "@react-keycloak/ssr";
 import { KeycloakInstance } from "keycloak-js";
 import getConfig from "next/config";
 import React, { useEffect, useState } from "react";
-import { Container, Table } from "semantic-ui-react";
+import { Container, Icon, Table } from "semantic-ui-react";
 import { LaunchClient } from "../../api/launch/launch_grpc_web_pb";
-import { Empty, LaunchList, LaunchView } from "../../api/launch/launch_pb";
+import { Empty, LaunchView, OperateRequest } from "../../api/launch/launch_pb";
 
 const LaunchTable: React.FC = () => {
   const { publicRuntimeConfig } = getConfig();
@@ -12,6 +12,7 @@ const LaunchTable: React.FC = () => {
 
   const client = new LaunchClient(publicRuntimeConfig.launch);
   const [launches, setLaunches] = useState<LaunchView[]>([]);
+
   useEffect(() => {
     if (keycloak?.idToken && initialized) {
       const stream = client.listLaunch(new Empty(), {
@@ -24,6 +25,33 @@ const LaunchTable: React.FC = () => {
     }
   }, [initialized]);
 
+  const updateLaunch = (
+    workflowID: string,
+    runID: string,
+    operation: string
+  ) => {
+    const operateLaunchRequest = new OperateRequest();
+    operateLaunchRequest.setWorkflowId(workflowID);
+    operateLaunchRequest.setRunId(runID);
+    operateLaunchRequest.setOperation(operation);
+    if (initialized && keycloak?.idToken) {
+      client.operateLaunch(
+        operateLaunchRequest,
+        {
+          authorization: "Bearer " + keycloak?.idToken,
+        },
+        (err, response) => {
+          if (err !== null) {
+            console.log(err);
+            return;
+          }
+          // setResult(true);
+          console.log("Launch updated");
+        }
+      );
+    }
+  };
+
   return (
     <Container>
       <Table>
@@ -31,6 +59,9 @@ const LaunchTable: React.FC = () => {
           <Table.Row>
             <Table.HeaderCell>Name</Table.HeaderCell>
             <Table.HeaderCell>Namespace</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
+
+            <Table.HeaderCell>Operations</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -38,6 +69,27 @@ const LaunchTable: React.FC = () => {
             <Table.Row>
               <Table.Cell>{e.getName()}</Table.Cell>
               <Table.Cell>{e.getNamespace()}</Table.Cell>
+              <Table.Cell>{e.getWorkloadStatus()}</Table.Cell>
+              <Table.Cell>
+                <Icon
+                  onClick={() =>
+                    updateLaunch(e.getWorkflowId(), e.getRunId(), "DELETE")
+                  }
+                  name="delete"
+                />
+                <Icon
+                  onClick={() =>
+                    updateLaunch(e.getWorkflowId(), e.getRunId(), "START")
+                  }
+                  name="play"
+                />
+                <Icon
+                  onClick={() =>
+                    updateLaunch(e.getWorkflowId(), e.getRunId(), "STOP")
+                  }
+                  name="stop"
+                />
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>

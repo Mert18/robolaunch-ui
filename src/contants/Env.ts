dd4838cb04aaf938bb37f614b5434b6aca81b8f0
mert18 <mertuygur02@gmail.com>
2022-03-03 08:35:38 +0300

    constants set, keycloak variables are taken from ./constants folder now.

--- src/contants/Env.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Environment variables
- */
-export const Env = {
-  NODE_ENV: process.env.NODE_ENV,
-};
--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers";
-import { Env } from "../contants";
+import { Env } from "../constants";
 
 /**
  * @see https://redux-toolkit.js.org/usage/usage-with-typescript#correct-typings-for-the-dispatch-type
